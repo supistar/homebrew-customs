7e796ac347d7f89d5bcdc47b9d787ca515f1930e
supistar <supistars@gmail.com>
2015-06-12 15:29:36 +0900

    Add Unity v4.6.3-p2 and Mono MDK 3.12.1 casks.

---
+++ Casks/mono-mdk-3-12-1.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'mono-mdk-3-12-1' do
+  version '3.12.1'
+  sha256 '98abc2854e9d0dfafd16c2b9dd27b82eda9e28440f112eded882d51cb56b89d8'
+
+  url "http://download.mono-project.com/archive/#{version}/macos-10-x86/MonoFramework-MDK-#{version}.macos10.xamarin.x86.pkg"
+  homepage 'http://mono-project.com/'
+  license :oss
+
+  pkg "MonoFramework-MDK-#{version}.macos10.xamarin.x86.pkg"
+  uninstall :pkgutil => 'com.xamarin.mono-MDK.pkg'
+end
---
+++ Casks/unity-4-6-3-p2.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'unity-4-6-3-p2' do
+  version '4.6.3p2'
+  sha256 'b2c86867864ab9fea636f4d4e91dead95ad20da8b245b117276b1952e60f4af9'
+
+  url "http://beta.unity3d.com/download/0553181569/unity_update-4.6.3p2.dmg"
+  homepage 'http://unity3d.com/unity/'
+  license :commercial
+
+  pkg 'Unity Update.pkg'
+
+  uninstall :pkgutil => 'com.unity3d.*'
+end
